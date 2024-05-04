758eb8e148223d1a9a3f438e73949ba3dc934f27
jayawardhana22 <madushanjayawardhana856@gmail.com>
2024-05-04 21:55:43 +0530

    Add menu and buy page

---
+++ app6k.js
@@ -0,0 +1,92 @@
+let product = {
+    name: 'Boys Casual Shor',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias necessitatibus ratione consectetur architecto accusamus iste assumenda velit molestiae reiciendis facere',
+    image: 'k5.png',
+    price: 'RS.1900.00',
+    children: [
+        {color: ' brown', size: 'M', price: 'RS.1900.00'},
+        {color: ' brown', size: 'L', price: 'RS.1900.00'},
+        {color: ' brown', size: 'XL', price: 'RS.1900.00'},
+        {color: 'red', size: 'M', price: 'RS.1900.00'},
+        {color: 'red', size: 'L', price: 'RS.1900.00'},
+        {color: 'red', size: 'XL', price: 'RS.1900.00'},
+    ]
+}
+let title = document.getElementById('title');
+let description = document.getElementById('description');
+let image = document.getElementById('image');
+let price = document.getElementById('price');
+let colorHTML = document.getElementById('colors'); 
+let sizeHTML = document.getElementById('sizes'); 
+
+let option = {
+    size :  null,
+    color : null
+}
+
+const initApp = () => {
+    title.innerText = product.name;
+    description.innerText = product.description;
+    price.innerText = product.price;
+
+    // get colors
+    let colors = (product.children).map(product => product.color);
+    colors = Array.from(new Set(colors));
+    if(colors.length > 0){
+        colors.forEach(color => {
+            let li = document.createElement('li');
+            li.style.backgroundColor = color;
+            li.setAttribute('data-color', color);
+            colorHTML.appendChild(li);
+            li.addEventListener('click', () => {
+                option.color = option.color !== color ? color : null;
+                refreshInfo();
+            })
+        })
+    }
+    // get sizes
+    let sizes = (product.children).map(product => product.size);
+    sizes = Array.from(new Set(sizes));
+    if(sizes.length > 0){
+        sizes.forEach(size => {
+            let li = document.createElement('li');
+            li.innerText = size;
+            li.setAttribute('data-size', size);
+            sizeHTML.appendChild(li);
+            li.addEventListener('click', () => {
+                option.size = option.size !== size ? size : null;
+                refreshInfo();
+            })
+        })
+    }
+}
+initApp();
+const refreshInfo = () => {
+    // colors
+    colorOldActive = colorHTML.querySelector('li.active');
+    if (colorOldActive) colorOldActive.classList.remove('active');
+    if(option.color !== null){
+        let colorNewActive = colorHTML.querySelector('li[data-color="'+option.color+'"]');
+        colorNewActive.classList.add('active');
+    }
+    // size
+    sizeOldActive = sizeHTML.querySelector('li.active');
+    if (sizeOldActive) sizeOldActive.classList.remove('active');
+    if(option.size !== null){
+        let sizeNewActive = sizeHTML.querySelector('li[data-size="'+option.size+'"]');
+        sizeNewActive.classList.add('active');
+    }
+
+    // set price
+    if(option.size === null || option.color === null){
+        price.innerText = product.price;
+    }else{
+        let childFound = (product.children).filter(product => {
+            return (product.size == option.size && product.color == option.color);
+        })[0];
+        price.innerText = childFound.price;
+    }
+}
+
+
+
